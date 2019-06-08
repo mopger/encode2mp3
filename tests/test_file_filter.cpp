278dfa0e7312f395b8e0f10540ce20d1a28cb743
mopger <no-reply>
2019-06-08 20:07:42 +0300

    add test_file_filter

---
+++ tests/test_file_filter.cpp
@@ -0,0 +1,11 @@
+#include "filesystem.hpp"
+#include "encode2mp3.hpp"
+
+//std::vector<std::string> extentions = {"wav", "pcm", "wave"};
+
+int main(int argc, char** args)
+{
+    //auto const& files = filterFiles(getCanonicalDirContents(args[1]), extentions);
+
+    return 0;
+}
